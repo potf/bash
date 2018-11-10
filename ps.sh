21bf2768c586ac7ea818d81f5d5aed29a606b785
roman <romanmvp@gmail.com>
2018-11-10 14:10:54 +0200

    Update ps.sh

--- ps.sh
+++ ps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-  2 
-  3 sleepTime=1
-  4 logFile="/var/log/PS.log"
-  5 
-  6 while true
-  7 
-  8 do
-  9     ps aux >> $logFile
- 10     echo 'working'
- 11     sleep $sleepTime
- 12 done
+
+sleepTime=1
+logFile="/var/log/PS.log"
+
+while true
+
+do
+	ps aux >> $logFile
+	echo 'working'
+	sleep $sleepTime
+done
