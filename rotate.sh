a522608eb3d31fd04261d91ce9e3d8edc261ac18
roman <romanmvp@gmail.com>
2018-11-10 15:30:03 +0200

    Update rotate.sh

--- rotate.sh
+++ rotate.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-confFile=`basename $1 .log` 
-echo "$1 {
+confFile="/etc/logrotate.d/`basename $1 .log`"
+
+if [[ ! -f $1 ]]
+    then
+        echo 'err'
+    else
+    echo "$1 {
     missingok
     nomail
     compress
     maxsize 1M
     rotate 1
-}" > /etc/logrotate.d/$confFile
+    }" > $confFile
+fi
