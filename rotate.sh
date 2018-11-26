2c0fc8f711e74a1fba342727cafdc35ca5fc1ce8
roman <romanmvp@gmail.com>
2018-11-26 17:36:50 +0200

    Update rotate.sh

--- rotate.sh
+++ rotate.sh
@@ -6,11 +6,11 @@ if [[ ! -f $1 ]]
     then
         echo 'err'
     else
-    echo "$1 {
-    missingok
-    nomail
-    compress
-    maxsize 1M
-    rotate 1
-    }" > $confFile
+        echo "$1 {
+        missingok
+        nomail
+        compress
+        maxsize 1M
+        rotate 1
+        }" > $confFile
 fi
