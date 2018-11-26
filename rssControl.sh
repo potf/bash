67f55edf42b89f4b436083477109c50de4623900
roman <romanmvp@gmail.com>
2018-11-26 17:31:28 +0200

    Update rssControl.sh

--- rssControl.sh
+++ rssControl.sh
@@ -4,11 +4,11 @@ sleepTimeSec=7
 checkParam='^[0-9]+$'
 
 if ! [[ $1 =~ $checkParam ]]; then
-	echo -e "Exeption: Invalid parameter, expected number\n\nUse: rssControl.sh [PARAMETER]"
+    echo -e "Exeption: Invalid parameter, expected number\n\nUse: rssControl.sh [PARAMETER]"
 else
-	while true
-	do
-		ps haxo pid,rss | awk '{if ($2 >= 5000) print $1}' | xargs kill -15
-		sleep $sleepTimeSec
-	done
+    while true
+    do
+        ps haxo pid,rss | awk '{if ($2 >= 5000) print $1}' | xargs kill -15
+        sleep $sleepTimeSec
+    done
 fi
