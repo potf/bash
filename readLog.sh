bfe63bb70367a3f48b76ea4fa6a972e5756c123a
roman <romanmvp@gmail.com>
2018-11-26 17:35:52 +0200

    Update readLog.sh

--- readLog.sh
+++ readLog.sh
@@ -1,8 +1,9 @@
-if [[ -f $3 && $1 =~ [0-2][0-9]:[0-9][0-9]:[0-9][0-9] && $2 =~ [0-2][0-9]:[0-9][0-9]:[0-9][0-9] ]]; then
-    first=`grep -n "$1" $3 | head -1 | cut -f1 -d:`
-    second=`grep -n "$2" $3 | head -1 | cut -f1 -d:`
+#!/bin/bash
 
-    sed -n -e ${first},${second}p < $3
+if [[ -f $3 && $1 =~ [0-2][0-9]:[0-9][0-9]:[0-9][0-9] && $2 =~ [0-2][0-9]:[0-9][0-9]:[0-9][0-9] ]]; then
+    timeFrom=`grep -n "$1" $3 | head -1 | cut -f1 -d:`
+    timeTo=`grep -n "$2" $3 | head -1 | cut -f1 -d:`
+    sed -n -e ${timeFrom},${timeTo}p < $3
 else
     echo -e "Exeption: invalid parameters\n\nUse: readLog.sh \"[TIMEFROM]\" \"[TIMETO]\" [LOGFILEPATH]\n\nExample: ./readLog.sh \"06:52:45\" \"06:56:53\" /var/log/yum.log"
 fi
