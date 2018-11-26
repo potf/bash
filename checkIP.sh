40b40d4a69d6fca9fb59ec73780428cc8e34f558
roman <romanmvp@gmail.com>
2018-11-26 17:32:01 +0200

    Update checkIP.sh

--- checkIP.sh
+++ checkIP.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [[ $1 =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-	ifsOriginal=$IFS
-	IFS='.'
-	ip=($1)
-	IFS=$ifsOriginal
-	echo Your ip address $1 is correct!
+    ifsOriginal=$IFS
+    IFS='.'
+    ip=($1)
+    IFS=$ifsOriginal
+    echo Your ip address $1 is correct!
 else 
-	echo -e "Exeption: invalid ip address\n\nUse: checkIP.sh [IPADDRESS]"
+    echo -e "Exeption: invalid ip address\n\nUse: checkIP.sh [IPADDRESS]"
 fi
